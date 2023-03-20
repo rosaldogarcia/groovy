9a57be881dc66f4e074a6f30e21d53c4dcc79aec
Rosaldo Garcia <rosaldo.garcia@hcl.com>
2023-03-20 18:31:31 +0800

    *

--- app1.groovy
+++ app1.groovy
@@ -1,12 +1,22 @@
-def map = [folder_app_1: folder_app_1, 
-            folder_app_2: folder_app_2
-            ]
+// def map = [folder_app_1: folder_app_1, 
+//             folder_app_2: folder_app_2
+//             ]
 
-        map.findAll { key, value -> 
-          if (value.contains("true")) {
-        // println key.replace("_","-")
-          //  def repoName = key.replace("_","-")
-          def repoName
-           repoName =  key.replace("_","-")
-          } 
+//         map.findAll { key, value -> 
+//           if (value.contains("true")) {
+//         // println key.replace("_","-")
+//           //  def repoName = key.replace("_","-")
+//           def repoName
+//            repoName =  key.replace("_","-")
+//           } 
+//         }
+
+node {
+    stage('Step1') {
+        if (env.BRANCH_NAME == 'main') {
+            echo 'Hello from main branch'
+        } else {
+            sh "echo 'Hello from ${env.BRANCH_NAME} branch!'"
         }
+    }
+}
